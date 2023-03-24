373dfb50beebee1deeb4df48931c25be349d9d20
Caio Espindola <caioespindola@hotmail.com>
2023-03-24 11:56:53 -0300

    start project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { AiOutlineSearch } from 'react-icons/ai'
+import '../src/styles.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="container">
+        <h1 className="title">Buscar CEP!</h1>
+
+        <div className="containerInput">
+          <input type="text" placeholder="Digite seu cep..." />
+
+          <button className="buttonSearch">
+            <AiOutlineSearch size={25} color="#000" />
+          </button>
+        </div>
+
+        <main className="main">
+          <h2>CEP: 098715000</h2>
+
+          <span>Rua: teste da silveira</span>
+          <span>Complemento teste</span>
+          <span>Tristan</span>
+          <span>Lut Golhien</span>
+        </main>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
